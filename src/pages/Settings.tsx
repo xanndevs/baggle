c3300238bb32dfc63f032d241bed4473588bfc86
xanndevs <info@xann.dev>
2025-07-30 08:11:48 +0300

    Added a modal that opens when the user clicks the FAB button. The modal allows users to input travel details and navigate through different steps of the travel creation process. (WIP)

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,23 +1,126 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import TravelsAccordion from '../components/TravelsAccordion';
-import './Settings.css';
+import React, { useState, useRef } from 'react';
+import {
+  IonButtons,
+  IonButton,
+  IonModal,
+  IonHeader,
+  IonContent,
+  IonToolbar,
+  IonTitle,
+  IonPage,
+  IonInput,
+  IonProgressBar,
+} from '@ionic/react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Settings: React.FC = () => {
+  const modal = useRef<HTMLIonModalElement>(null);
+
+  const [inputValue, setInputValue] = useState('');
+  const [progress, setProgress] = useState<number>(0);
+  const [modalPage, setModalPage] = useState(1);
+
+  const setModal = () => {
+    setProgress((previous) => previous + 0.05);
+  };
+
+  const pageVariants = {
+    initial: { opacity: 0, x: 50 },
+    animate: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: -50 },
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Settings</IonTitle>
+          <IonTitle>App</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Settings</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <TravelsAccordion />
+      <IonContent className="ion-padding">
+        <IonButton id="open-modal" expand="block" onClick={setModal}>
+          Open Bottom Sheet
+        </IonButton>
+
+        <IonModal
+          ref={modal}
+          trigger="open-modal"
+          initialBreakpoint={0.85}
+          breakpoints={[0, 0.85]}
+          backdropBreakpoint={0.1}
+          canDismiss={true}
+          handleBehavior="none"
+          onWillDismiss={() => setModalPage(1)}
+        >
+          <IonHeader >
+            <IonToolbar>
+              <IonTitle>New Travel</IonTitle>
+              <IonButton slot='end' fill='clear' size='small' onClick={() => modal.current?.dismiss()}>Cancel</IonButton>
+            </IonToolbar>
+              <IonProgressBar value={progress}></IonProgressBar>
+          </IonHeader>
+            <IonContent className="ion-padding">
+
+              <AnimatePresence mode="wait">
+                {modalPage === 1 && (
+                  <motion.div
+                    key="page1"
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    variants={pageVariants}
+                    transition={{ duration: 0.2 }}
+                  >
+                    <IonInput
+                      label="Travel Name"
+                      labelPlacement="floating"
+                      fill="outline"
+                      value={inputValue}
+                      onIonChange={(e) => setInputValue(e.detail.value!)}
+                    />
+                    <IonButton
+                      expand="block"
+                      className="ion-margin-top"
+                      onClick={() => {setModalPage(2); setProgress(0.5);}}
+                    >
+                      Next
+                    </IonButton>
+                  </motion.div>
+                )}
+
+                {modalPage === 2 && (
+                  <motion.div
+                    key="page2"
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    variants={pageVariants}
+                    transition={{ duration: 0.3 }}
+                  >
+                    <h2>Review Input</h2>
+                    <p>You entered: {inputValue}</p>
+                    <IonButton
+                      expand="block"
+                      className="ion-margin-bottom"
+                      onClick={() => setModalPage(1)}
+                    >
+                      Back
+                    </IonButton>
+                    <IonButton
+                      expand="block"
+                      color="success"
+                      onClick={() => {
+                        console.log('Input value:', inputValue);
+                        modal.current?.dismiss();
+                      }}
+                    >
+                      Submit
+                    </IonButton>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </IonContent>
+        </IonModal>
       </IonContent>
     </IonPage>
   );
--- src/pages/Travels.tsx
+++ src/pages/Travels.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useRef, useState } from 'react';
+import {
+  IonContent, IonFab, IonFabButton, IonFabList, IonHeader, IonIcon, IonModal, IonPage, IonTitle, IonToolbar, IonButtons,
+  IonButton,
+  IonInput,
+  IonProgressBar,
+} from '@ionic/react';
 import TravelsAccordion from '../components/TravelsAccordion';
 import './Travels.css';
+import { addSharp } from 'ionicons/icons';
+import { motion, AnimatePresence } from 'framer-motion';
+
 
 const Travels: React.FC = () => {
+
+  const modal = useRef<HTMLIonModalElement>(null);
+
+  const [inputValue, setInputValue] = useState('');
+  const [progress, setProgress] = useState<number>(0);
+  const [modalPage, setModalPage] = useState(1);
+
+  const setModal = () => {
+    setProgress((previous) => previous + 0.05);
+  };
+
+  const pageVariants = {
+    initial: { opacity: 0, x: 50 },
+    animate: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: -50 },
+  };
+
+
   return (
     <IonPage>
       <IonHeader>
@@ -18,6 +44,92 @@ const Travels: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <TravelsAccordion />
+
+        <IonFab slot="fixed" vertical="bottom" horizontal="end" id="open-modal">
+          <IonFabButton onClick={setModal}>
+            <IonIcon icon={addSharp}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+          <IonModal
+            ref={modal}
+            trigger="open-modal"
+            initialBreakpoint={0.85}
+            breakpoints={[0, 0.85]}
+            backdropBreakpoint={0.1}
+            canDismiss={true}
+            handleBehavior="none"
+            onWillDismiss={() => setModalPage(1)}
+          >
+            <IonHeader >
+              <IonToolbar>
+                <IonTitle>New Travel</IonTitle>
+                <IonButton slot='end' fill='clear' size='small' onClick={() => modal.current?.dismiss()}>Cancel</IonButton>
+              </IonToolbar>
+              <IonProgressBar value={progress}></IonProgressBar>
+            </IonHeader>
+            <IonContent className="ion-padding">
+
+              <AnimatePresence mode="wait">
+                {modalPage === 1 && (
+                  <motion.div
+                    key="page1"
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    variants={pageVariants}
+                    transition={{ duration: 0.2 }}
+                  >
+                    <IonInput
+                      label="Travel Name"
+                      labelPlacement="floating"
+                      fill="outline"
+                      value={inputValue}
+                      onIonChange={(e) => setInputValue(e.detail.value!)}
+                    />
+                    <IonButton
+                      expand="block"
+                      className="ion-margin-top"
+                      onClick={() => { setModalPage(2); setProgress(0.5); }}
+                    >
+                      Next
+                    </IonButton>
+                  </motion.div>
+                )}
+
+                {modalPage === 2 && (
+                  <motion.div
+                    key="page2"
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    variants={pageVariants}
+                    transition={{ duration: 0.3 }}
+                  >
+                    <h2>Review Input</h2>
+                    <p>You entered: {inputValue}</p>
+                    <IonButton
+                      expand="block"
+                      className="ion-margin-bottom"
+                      onClick={() => setModalPage(1)}
+                    >
+                      Back
+                    </IonButton>
+                    <IonButton
+                      expand="block"
+                      color="success"
+                      onClick={() => {
+                        console.log('Input value:', inputValue);
+                        modal.current?.dismiss();
+                      }}
+                    >
+                      Submit
+                    </IonButton>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </IonContent>
+          </IonModal>
+
       </IonContent>
     </IonPage>
   );
